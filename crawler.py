dfcd9176cece375efc7ce3c7ca962aa3d0ec63cd
tina.leunggg <tinaisworking@gmail.com>
2025-11-11 20:30:15 -0800

    Crawler: use context manager for scrape function

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from crawl4ai.content_filter_strategy import PruningContentFilter
 import asyncio
 
 class Crawler:
-    ''' crawl individual news article page'''
+
     def __init__(self):
         self.browser_conf = BrowserConfig(
             browser_type="chromium",
@@ -22,28 +22,21 @@ class Crawler:
             markdown_generator=self.markdown_generator,
             word_count_threshold=10,
         )
-       
-        self.crawler = AsyncWebCrawler(config=self.browser_conf)
-        
-    async def start(self):
-        await self.crawler.start()
-        
-    async def close(self):
-        await self.crawler.close()
-        
+            
     async def scrape(self, urls):
         try:
-            results = await self.crawler.arun_many(
-                urls,
-                config=self.run_conf
-            )
-            
-            for result in results:
-                if result.success and result.markdown:
-                    print('✅ Scraping successful')
-                    return result.markdown.fit_markdown
-                else:
-                    raise Exception('Scraping failed - no content returned')
+            async with AsyncWebCrawler(config=self.browser_conf) as crawler:
+                results = await crawler.arun_many(
+                    urls,
+                    config=self.run_conf
+                )
+                
+                for result in results:
+                    if result.success and result.markdown:
+                        print('✅ Scraping successful')
+                        return result.markdown.fit_markdown
+                    else:
+                        raise Exception('Scraping failed - no content returned')
                 
         except Exception as error:
             print(f'❌ Crawl4AI error for {urls}: {error}')
@@ -51,11 +44,9 @@ class Crawler:
 if __name__ == "__main__":
     async def main():
         crawler = Crawler()
-        await crawler.start()
         result = await crawler.scrape(["https://www.geeksforgeeks.org/python/python-features/"])
-        with open("fit_markdown_reult.txt.", "w") as f:
-            f.write(result)        
-        await crawler.close()
+        with open("fit_markdown_geeksforgeeks.txt.", "w", encoding='utf-8') as f:
+            f.write(str(result))        
         
     asyncio.run(main())
         
